e1c74b5820f07c15c478b681f9f301bd0b0ad8de
imason5 <ianmason5@googlemail.com>
2023-05-17 17:09:23 +0100

    Add signup, login, front and backend

--- back/routes/auth.routes.js
+++ back/routes/auth.routes.js
@@ -1,41 +1,75 @@
-const router = require('express').Router()
-const jwt = require('jsonwebtoken')
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/signup', (req, res, next) => {
+const User = require("../models/User.model");
+
+// ******* SIGNUP ROUTE *******
+router.post("/signup", async (req, res, next) => {
   /* Get back the payload from your request, as it's a POST you can access req.body */
+  const { email, password } = req.body;
 
+  // Check if email and password are provided
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
+
+  // Check if user already exists
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return res.status(400).json({ message: "User already exists" });
+  }
   /* Hash the password using bcryptjs */
-
+  const salt = await bcrypt.genSalt(13);
+  const hashedPassword = await bcrypt.hash(password, salt);
   /* Record your user to the DB */
+  const newUser = new User({ email, password: hashedPassword });
+  await newUser.save();
 
-  res.json('Pinging signup')
-})
+  res.status(201).json({ message: "User registered successfully" });
+});
 
-router.post('/login', (req, res, next) => {
+// ******* LOGIN ROUTE *******
+router.post("/login", async (req, res, next) => {
   /* Get back the payload from your request, as it's a POST you can access req.body */
-
+  const { email, password } = req.body;
+  // Check if email and password are provided
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
   /* Try to get your user from the DB */
 
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
   /* If your user exists, check if the password is correct */
+
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
 
   /* If your password is correct, sign the JWT using jsonwebtoken */
   const authToken = jwt.sign(
     {
-      expiresIn: '6h',
-      user: null, // Put yhe data of your user in there
+      userId: user._id,
     },
     process.env.TOKEN_SECRET,
     {
-      algorithm: 'HS256',
+      expiresIn: "6h",
+      algorithm: "HS256",
     }
-  )
+  );
 
-  res.json('Pinging login')
-})
+  res.json({ message: "Login successful", token: authToken });
+});
 
-router.get('/verify', (req, res, next) => {
+router.get("/verify", (req, res, next) => {
   // You need to use the middleware there, if the request passes the middleware, it means your token is good
-  res.json('Pinging verify')
-})
+  res.json("Pinging verify");
+});
 
-module.exports = router
+module.exports = router;
--- front/src/pages/SignupPage.jsx
+++ front/src/pages/SignupPage.jsx
@@ -1,45 +1,73 @@
-import { Box, Button, PasswordInput, Text, TextInput } from '@mantine/core'
+import { useState } from "react";
+import axios from "axios";
+import { Box, Button, PasswordInput, Text, TextInput } from "@mantine/core";
 
 const SignupPage = () => {
-  // Add some states to control your inputs
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleSubmit = event => {
-    event.preventDefault()
-    // Send your signup information to your backend
-  }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:5005/api/signup", {
+        email,
+        password,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <Box
       sx={{
-        margin: '0 auto',
-        maxWidth: '400px',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        height: 'calc(100vh - 100px)',
+        margin: "0 auto",
+        maxWidth: "400px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        height: "calc(100vh - 100px)",
       }}
     >
-      <Text align='center' size='xl' weight='bold'>
+      <Text align="center" size="xl" weight="bold">
         Signup
       </Text>
       <Box
-        component='form'
-        sx={{ display: 'flex', flexDirection: 'column', gap: '20px', marginTop: '2rem' }}
+        component="form"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "20px",
+          marginTop: "2rem",
+        }}
         onSubmit={handleSubmit}
       >
-        <TextInput label='Username' variant='filled' withAsterisk />
-        <PasswordInput label='Password' variant='filled' withAsterisk />
+        <TextInput
+          label="Email"
+          variant="filled"
+          withAsterisk
+          value={email || ""}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <PasswordInput
+          label="Password"
+          variant="filled"
+          withAsterisk
+          value={password || ""}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <Button
-          type='submit'
-          variant='filled'
-          color='cyan'
-          sx={{ marginTop: '1rem', alignSelf: 'center' }}
+          type="submit"
+          variant="filled"
+          color="cyan"
+          sx={{ marginTop: "1rem", alignSelf: "center" }}
         >
           Register
         </Button>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default SignupPage
+export default SignupPage;
